470711b2a5cdcc098d3b7a4810d4269c5ead2499
André Pestana Silva <atlas.ifpa@gmail.com>
2023-03-06 16:57:10 -0300

    feat: cria os arquétipos básicos

---
+++ src/Archetypes/Warrior.ts
@@ -0,0 +1,21 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+export default class Warrior extends Archetype {
+  private _energyType: EnergyType;
+  private static _population = 0;
+
+  constructor(name: string) {
+    super(name);
+    this._energyType = 'stamina';
+    Warrior._population += 1;
+  }
+
+  static createdArchetypeInstances() {
+    return this._population;
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+}
